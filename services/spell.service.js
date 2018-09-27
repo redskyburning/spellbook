02ded2ffa40060eadf6bd2a0db7a5523c291a280
Ben Webb <ben@redskyburning.com>
2018-09-27 10:00:01 -0400

    upgraded to nuxt 2, fixed init issue

--- services/spell.service.js
+++ services/spell.service.js
@@ -1,36 +1,54 @@
 import PouchDB from 'pouchdb';
-import * as find from 'pouchdb-find';
+//import * as find from 'pouchdb-find';
 
 export default class SpellService {
   constructor($axios) {
     this.$axios = $axios;
 
-    PouchDB.plugin(find);
+    //PouchDB.plugin(find);
 
     this.DB_NAME = 'db/spells';
     this.db      = new PouchDB(this.DB_NAME);
+    this.initPromise = null;
 
-    this.db.info()
-      .then((info) => {
-        if(info.doc_count === 0) {
-          this.initDB()
-            .then(() => {
-              console.info('DB initialized');
-            })
-            .catch((error) => {
-              console.error('Error initing db', error);
-            });
-        } else {
-          console.info('DB already initialized, moving on.');
-        }
+    this.initDB()
+      .then((message) => {
+        console.info(message);
       })
       .catch((error) => {
-        console.error('Error getting db info', error);
+        console.error('error');
       });
-
   }
 
-  initDB(){
+  initDB() {
+    if(this.initPromise === null) {
+      this.initPromise = new Promise((resolve,reject) => {
+        this.db.info()
+          .then((info) => {
+            if(info.doc_count === 0) {
+              this.populateDB()
+                .then(() => {
+                  resolve('DB initialized');
+                })
+                .catch((error) => {
+                  console.error('Error initing db');
+                  reject(error);
+                });
+            } else {
+              resolve('DB already initialized, moving on.');
+            }
+          })
+          .catch((error) => {
+            console.error('Error getting db info');
+            reject(error);
+          });
+      });
+    }
+
+    return this.initPromise;
+  }
+
+  populateDB(){
     return new Promise((resolve,reject) => {
       if(this.$axios) {
         this.$axios.get('/data/spells.json')
@@ -53,18 +71,38 @@ export default class SpellService {
   }
 
   getSpellByKey(key) {
-    return this.db.get(key);
+    return new Promise((resolve,reject) => {
+      this.initDB()
+        .then(() => {
+          this.db.get(key)
+            .then((spell) => {
+              resolve(spell);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   }
 
   getAllSpells() {
     return new Promise((resolve,reject) => {
-      this.db.allDocs({include_docs: true})
-        .then((results) => {
-          if(results && Array.isArray(results.rows)) {
-            resolve(results.rows.map(row => row.doc));
-          } else {
-            reject('Malformed response in getAllSpells()');
-          }
+      this.initDB()
+        .then(() => {
+          this.db.allDocs({include_docs: true})
+            .then((results) => {
+              if(results && Array.isArray(results.rows)) {
+                resolve(results.rows.map(row => row.doc));
+              } else {
+                reject('Malformed response in getAllSpells()');
+              }
+            })
+            .catch((error) => {
+              reject(error);
+            });
         })
         .catch((error) => {
           reject(error);
