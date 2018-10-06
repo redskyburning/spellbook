6db0ab190d638ae68c16d2fe369495742f18fc2a
Ben Webb <ben@redskyburning.com>
2018-10-06 12:31:56 -0400

    componentized spell filters

--- pages/index.vue
+++ pages/index.vue
@@ -4,57 +4,7 @@
 
 <template>
   <section class="spellbook">
-    <div class="spellbook__search">
-			<div class="spellbook__search__inner">
-				<b-field label="Name">
-					<b-input v-model="query"></b-input>
-				</b-field>
-
-				<b-field label="Spell Level">
-					<b-select placeholder="Spell Level"
-										v-model="selectedLevel">
-						<option v-for="(level, levelIndex) in levels"
-										:key="levelIndex"
-										:value="levelIndex">{{ level }}</option>
-					</b-select>
-				</b-field>
-
-				<b-field label="Arcane School">
-					<b-select v-model="selectedSchool">
-						<option v-for="(schoolLabel, schoolKey) in schools"
-										:key="schoolKey"
-										:value="schoolKey">{{ schoolLabel }}</option>
-					</b-select>
-				</b-field>
-
-				<b-field label="Ritual">
-					<b-switch v-model="isRitual">
-						{{ isRitual ? 'Only Rituals' : '' }}
-					</b-switch>
-				</b-field>
-
-				<b-field label="Concentration">
-					<b-switch v-model="concentration"></b-switch>
-				</b-field>
-
-				<b-field label="Filter By Spellbook?">
-					<b-switch v-model="filterBooks">
-						{{ filterBooks ? 'Yes' : 'No' }}
-					</b-switch>
-				</b-field>
-
-				<div v-if="filterBooks">
-					<div class="label">Spellbooks</div>
-					<div v-for="bookKey in spellbooks"
-							 :key="bookKey">
-						<b-checkbox v-model="selectedBooks"
-												:native-value="bookKey">
-							{{ parseSpellbookKey(bookKey) }}
-						</b-checkbox>
-					</div>
-				</div>
-			</div>
-    </div>
+		<spell-filters class="spellbook__search"></spell-filters>
 		<spell-list class="spellbook__results"
 								:spells="$store.state.spells"></spell-list>
   </section>
