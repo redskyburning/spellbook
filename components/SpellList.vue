1bb1684b1c90ece0ac1547d89029251d0aef400e
Ben Webb <ben@redskyburning.com>
2018-10-06 16:47:37 -0400

    so much trial and error for one modal!

--- components/SpellList.vue
+++ components/SpellList.vue
@@ -10,8 +10,26 @@
 		<div class="spell-list__no-results" v-if="$store.state.spells.length < 1">No spells found!</div>
 
 		<b-modal :active.sync="castModalActive">
-			<cast-modal :spell="selectedSpell"
-									@close-modal="onCastClose"></cast-modal>
+			<div class="cast-modal" v-if="selectedSpell">
+				<div class="cast-modal__title">
+					Cast {{ selectedSpell.name }}?
+				</div>
+				<div class="cast-modal__level">
+					<b-field>
+						<p class="control">
+							<span class="button is-static">@Level</span>
+						</p>
+						<b-select v-model="selectedLevel">
+							<option v-for="(level, levelId) in getPossibleLevels(selectedSpell)"
+											:key="levelId"
+											:value="level">{{ level|levelLabel }}</option>
+						</b-select>
+					</b-field>
+				</div>
+				<div class="cast-modal__actions">
+					<button class="cast-modal__action button is-large is-primary" @click="onCastClose()">Cast!</button>
+				</div>
+			</div>
 		</b-modal>
 	</div>
 </template>
@@ -36,8 +54,15 @@
 			}
 		},
 		methods   : {
+			getPossibleLevels(spell) {
+				let levels   = Object.keys(this.$store.state.castingSlots);
+				let maxLevel = levels[levels.length - 1];
+
+				return [1,2,3,4,5,6,7,8,9].slice(spell.level - 1, maxLevel);
+			},
 			onCastClose(level) {
-				console.warn('???',level);
+				this.cast(this.selectedSpell,this.selectedLevel);
+				this.castModalActive = false;
 			},
 			openCastModal(spell) {
 				this.selectedSpell   = spell;
@@ -53,7 +78,7 @@
 						let message = `${spell.name} cast` + ((spell.level !== level) ? ` at level ${level}` : '');
 
 						this.$toast.open({
-							duration: 1000,
+							duration: 1500,
 							message : message,
 							position: 'is-bottom',
 							queue   : false,
@@ -62,7 +87,7 @@
 					})
 					.catch((error) => {
 						this.$toast.open({
-							duration: 1000,
+							duration: 1500,
 							message : error,
 							position: 'is-bottom',
 							queue   : false,
